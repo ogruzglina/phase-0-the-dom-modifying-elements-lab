c8797cfd803184d9a44f7886bbff95ea4d672e16
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-16 13:12:35 -0500

    commit tree f6dbcdc15bb1e0593c6ba7881901f0f646014ad7

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const main = document.getElementById("main");
 main.remove();
 
 const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
+//newHeader.setAttribute("id", "victory");
+//or
+newHeader.id = 'victory';
 newHeader.textContent = "Olga is the champion";
+//or
+//newHeader.append('Olga is the champion');
+const parent = document.querySelector('body');
+parent.appendChild(newHeader);
